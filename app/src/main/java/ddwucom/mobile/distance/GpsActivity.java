5dfc68449b260ac233434439600d9788184ef9a2
Hyeonjikorea <61769743+Hyeonjikorea@users.noreply.github.com>
2020-07-30 18:39:32 +0900

    [add] layout_gps.xml [add] nav_header.xml [add] navigation_menu.xml [add] GpsActivity

---
+++ app/src/main/java/ddwucom/mobile/distance/GpsActivity.java
@@ -0,0 +1,70 @@
+package ddwucom.mobile.distance;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class GpsActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private Context context = this;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_gps);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
+        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon); //뒤로가기 버튼 이미지 지정
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                int id = menuItem.getItemId();
+                String title = menuItem.getTitle().toString();
+
+                if(id == R.id.account){
+                    Toast.makeText(context, title + ": 계정 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.setting){
+                    Toast.makeText(context, title + ": 설정 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.logout){
+                    Toast.makeText(context, title + ": 로그아웃 시도중", Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/ddwucom/mobile/distance/MainActivity.java
+++ app/src/main/java/ddwucom/mobile/distance/MainActivity.java
@@ -2,20 +2,33 @@ package ddwucom.mobile.distance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText login_et_id;
+    EditText login_et_pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 안녕하세요..
-        int value;
-        int val;
-        int value3;
+       login_et_id = findViewById(R.id.login_et_id);
+       login_et_pw = findViewById(R.id.login_et_pw);
 
-        int value4;
+
+    }
+
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.login_btn:
+                Intent intent = new Intent(this, GpsActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
