af6daee4d4aadb5ad923fb06d183677e935bf4c4
jaeim <68433884+jaeim@users.noreply.github.com>
2020-08-31 20:13:09 +0900

    [delete]jsoup.jar

--- app/src/main/java/ddwucom/mobile/distance/ConfirmedCaseActivity.java
+++ app/src/main/java/ddwucom/mobile/distance/ConfirmedCaseActivity.java
@@ -15,7 +15,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.nio.file.Path;
+
 import java.util.ArrayList;
 
 public class ConfirmedCaseActivity extends AppCompatActivity {
