791bc98dbaeb38beadadf2de29309b9aa52eb10e
82108 <shinsubin10won@gmail.com>
2020-09-11 02:13:36 +0900

    [UPDATE] MovinInfo, GpsActivity, ConfirmedCaseActivity 수정 [ADD] 확진자 동선 현황 레이아웃 추가

--- app/src/main/java/ddwucom/mobile/distance/MovingInfo.java
+++ app/src/main/java/ddwucom/mobile/distance/MovingInfo.java
@@ -6,9 +6,9 @@ import java.util.Date;
 public class MovingInfo {
 
     int id;
-//    int year;
-//    int month;
-//    int dayOfMonth;
+    int year;
+    int month;
+    int dayOfMonth;
     String startTime;
     String endTime;
     String location;
@@ -20,7 +20,10 @@ public class MovingInfo {
         endTime = null;
     }
 
-    public MovingInfo(String startTime, String endTime, double latitude, double longitude) {
+    public MovingInfo(int year, int month, int dayOfMonth, String startTime, String endTime, double latitude, double longitude) {
+        this.year = year;
+        this.month = month;
+        this.dayOfMonth = dayOfMonth;
         this.startTime = startTime;
         this.endTime = endTime;
 //        this.location = location;
@@ -36,29 +39,29 @@ public class MovingInfo {
         this.id = id;
     }
 
-//    public int getYear() {
-//        return year;
-//    }
-//
-//    public void setYear(int year) {
-//        this.year = year;
-//    }
-//
-//    public int getMonth() {
-//        return month;
-//    }
-//
-//    public void setMonth(int month) {
-//        this.month = month;
-//    }
-//
-//    public int getDayOfMonth() {
-//        return dayOfMonth;
-//    }
-//
-//    public void setDayOfMonth(int dayOfMonth) {
-//        this.dayOfMonth = dayOfMonth;
-//    }
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDayOfMonth() {
+        return dayOfMonth;
+    }
+
+    public void setDayOfMonth(int dayOfMonth) {
+        this.dayOfMonth = dayOfMonth;
+    }
 
     public String getStartTime() {
         return startTime;
