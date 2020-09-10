d6b705b78c0e5377e507be59e5e7fea3bd193e96
Hyeonjikorea <61769743+Hyeonjikorea@users.noreply.github.com>
2020-09-11 01:01:09 +0900

    [delete] layout_moving_all.xml, layout_moving_calendar.xml, layout_moving_location.xml
    [update] activity_moving.xml, MovingActivity.java

--- app/src/main/java/ddwucom/mobile/distance/MovingActivity.java
+++ app/src/main/java/ddwucom/mobile/distance/MovingActivity.java
@@ -1,5 +1,6 @@
 package ddwucom.mobile.distance;
 
+import android.app.FragmentManager;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -8,13 +9,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CalendarView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MovingActivity extends AppCompatActivity {
 
@@ -25,8 +34,16 @@ public class MovingActivity extends AppCompatActivity {
     ListView all_listView;
     ListView calendar_listView;
     Cursor cursor;
-    ConstraintLayout layout_moving;
+    FrameLayout layout_moving;
     LayoutInflater inflater;
+
+    LinearLayout all_layout;
+    LinearLayout calendar_layout;
+    LinearLayout map_layout;
+
+    FragmentManager fragmentManager;
+    MapFragment mapFragment;
+    GoogleMap map;
 
     private static final String TAG = "MovingActivity";
 
@@ -42,6 +59,40 @@ public class MovingActivity extends AppCompatActivity {
         cursor = manager.getAllInfos();
         adapter = new MovingInfoAdapter(MovingActivity.this, R.layout.layout_listview, cursor);
 
+        all_layout = findViewById(R.id.all_layout);
+        calendar_layout = findViewById(R.id.calendar_layout);
+        map_layout = findViewById(R.id.map_layout);
+
+        all_listView = findViewById(R.id.all_listView);
+        all_listView.setAdapter(adapter);
+
+        calendar_listView = findViewById(R.id.calendar_listView);
+        calendar_listView.setAdapter(adapter);
+
+        calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+                Log.d(TAG, year + ", " + month + ", " + dayOfMonth);
+                Cursor cursor = manager.searchWithDate(year, month, dayOfMonth);
+                adapter.changeCursor(cursor);
+
+            }
+        });
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.moving_map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                LatLng standard = new LatLng(37.519576, 126.940245);
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(standard).zoom(16).build();
+                map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
         spinner = findViewById(R.id.search_spinner);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -50,38 +101,25 @@ public class MovingActivity extends AppCompatActivity {
                     case 0:
                         adapter.changeCursor(manager.getAllInfos());
 
-                        layout_moving.removeAllViews();
-                        inflater.inflate(R.layout.layout_moving_all, layout_moving, true);
-
-                        all_listView = findViewById(R.id.all_listView);
-                        all_listView.setAdapter(adapter);
+                        all_layout.setVisibility(View.VISIBLE);
+                        calendar_layout.setVisibility(View.INVISIBLE);
+                        map_layout.setVisibility(View.INVISIBLE);
 
                         break;
                     case 1:
                         adapter.changeCursor(null);
 
-                        layout_moving.removeAllViews();
-                        inflater.inflate(R.layout.layout_moving_calendar, layout_moving, true);
-
-                        calendar_listView = findViewById(R.id.calendar_listView);
-                        calendar_listView.setAdapter(adapter);
-
-                        calendarView = findViewById(R.id.calendarView);
-                        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-                            @Override
-                            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-
-                                Log.d(TAG, year + ", " + month + ", " + dayOfMonth);
-                                Cursor cursor = manager.searchWithDate(year, month, dayOfMonth);
-                               adapter.changeCursor(cursor);
-
-                            }
-                        });
+                        all_layout.setVisibility(View.INVISIBLE);
+                        calendar_layout.setVisibility(View.VISIBLE);
+                        map_layout.setVisibility(View.INVISIBLE);
 
                         break;
                     case 2:
-                        layout_moving.removeAllViews();
-                        inflater.inflate(R.layout.layout_moving_location, layout_moving, true);
+
+                        all_layout.setVisibility(View.INVISIBLE);
+                        calendar_layout.setVisibility(View.INVISIBLE);
+                        map_layout.setVisibility(View.VISIBLE);
+
                         break;
                 }
             }
