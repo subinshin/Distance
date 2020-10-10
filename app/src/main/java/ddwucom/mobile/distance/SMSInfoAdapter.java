5ce50e6be259af78b22f74205e52d2099323d072
82108 <shinsubin10won@gmail.com>
2020-10-10 23:04:46 +0900

    [UPDATE] smsList 저장/삭제 기능 구현중

--- app/src/main/java/ddwucom/mobile/distance/SMSInfoAdapter.java
+++ app/src/main/java/ddwucom/mobile/distance/SMSInfoAdapter.java
@@ -5,8 +5,10 @@ import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SMSInfoAdapter extends CursorAdapter {
     Cursor cursor;
@@ -25,9 +27,16 @@ public class SMSInfoAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, final Cursor cursor) {
         TextView tv_datetime = view.findViewById(R.id.tv_datetime);
         TextView tv_location = view.findViewById(R.id.tv_location);
+        Button btn = view.findViewById(R.id.saveBtn);
+        btn.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(context, Integer.toString(cursor.getInt(0)), Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         tv_datetime.setText(cursor.getString(cursor.getColumnIndex(helper.COL_DATETIME)));
         tv_location.setText(cursor.getString(cursor.getColumnIndex(helper.COL_LOCATION)));
@@ -39,6 +48,19 @@ public class SMSInfoAdapter extends CursorAdapter {
 
         return v;
     }
+
+//    public View getView(int pos, View convertView, ViewGroup viewGroup) {
+//        final int position = pos;
+//
+//        Button btn = convertView.findViewById(R.id.saveBtn);
+//        btn.setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View view) {
+//                Toast.makeText(context, position, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        return convertView;
+//    }
 
     public Cursor getCursor(){
         return cursor;
