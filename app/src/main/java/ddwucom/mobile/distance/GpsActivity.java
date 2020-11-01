758f4a96c4decec9ef964afc9e8b4d1f9954a274
82108 <shinsubin10won@gmail.com>
2020-11-01 18:57:16 +0900

    [UPDATE] 오타 수정

--- app/src/main/java/ddwucom/mobile/distance/GpsActivity.java
+++ app/src/main/java/ddwucom/mobile/distance/GpsActivity.java
@@ -603,9 +603,9 @@ public class GpsActivity extends AppCompatActivity {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED
                         && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permission is granted!\n", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Permission is granted!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(this, "Permission is denied!\n", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Permission is denied!", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
